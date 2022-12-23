19256a96594c26cbca55c4b7a2b9131ef4b6d92b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-12-23 22:20:52 +0800

    [TEST] [Bugtask-003] - Fix About and Profile Page (#3)

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Profile = () => {
-  return <div>This is the Profile Page</div>
+  return (
+    <div>
+      This is the Profile Page
+      <span>Testing bugtask</span>
+    </div>
+  )
 }
 
 export default Profile
